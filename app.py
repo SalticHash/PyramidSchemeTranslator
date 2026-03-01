15b2107966b134832c8bb1f8f94180d26b211f6c
SalticHash <saltichash@gmail.com>
2026-02-28 22:06:38 -0600

    Add Flask App
    * Moved banner
    * Added translate() function to translate.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from translate import translate
+# builtins
+from io import BytesIO
+# Flask Imports
+from werkzeug.datastructures import FileStorage
+from flask import Flask, render_template, request, send_file, redirect, Response
+
+# Configure application
+app = Flask(__name__)
+
+def is_pdf(filename: str):
+    return filename.endswith(".pdf")
+           
+# Routes
+@app.route("/translate", methods=["POST"])
+def tranlate_poster() -> Response:
+    generated_posters: FileStorage = request.files["generated_posters"]
+    translated_poster: FileStorage = request.files["translated_poster"]
+    
+    pdf_bytes = translate(generated_posters.stream.read(), translated_poster.stream.read())
+
+    return send_file(
+        BytesIO(pdf_bytes),
+        mimetype="application/pdf",
+        as_attachment=False,
+        download_name="result.pdf"
+    )
+    
+@app.route("/", methods=["GET"])
+def index() -> str:
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5454)
--- translate.py
+++ translate.py
@@ -1,31 +1,35 @@
-import fitz
+from fitz import open as openPDF, Rect
+from io import BytesIO
 
+def translate(donor_bytes: BytesIO, translated_bytes: BytesIO) -> BytesIO:
+    donor_doc = openPDF(stream=donor_bytes, filetype="pdf")
+    translated_base = openPDF(stream=translated_bytes, filetype="pdf")
+    translated_doc = openPDF(stream=translated_bytes, filetype="pdf")
 
-donor_doc = fitz.open("test_bin/FlavorDono.pdf")
-translated_base = fitz.open("test_bin/FlavorTrad.pdf")
-translated_doc = fitz.open("test_bin/FlavorTrad.pdf")
-
-poster_count = len(donor_doc)
-
-for poster_i in range(poster_count):
-    pg = donor_doc[poster_i]
-    if poster_i > 0:
-        translated_doc.insert_pdf(translated_base, to_page=0, start_at=poster_i)
-    
-    qr_rect = None
-    qr_bytes = None
-    for img in pg.get_images(full=True):
-        name = img[7]
-        if len(name) != 40 or not name.startswith("FormXob."): continue
-        qr_rect = pg.get_image_bbox(img)
+    poster_count = len(donor_doc)
+    print(poster_count)
+    for poster_i in range(poster_count):
+        pg = donor_doc[poster_i]
+        if poster_i > 0:
+            translated_doc.insert_pdf(translated_base, to_page=0, start_at=poster_i)
+            print("Made new page on translated!")
         
-        xref = img[0]
-        img_dict = donor_doc.extract_image(xref)
-        qr_bytes = img_dict["image"]
-        break
+        qr_rect: Rect = None
+        qr_bytes: BytesIO = None
+        for img in pg.get_images(full=True):
+            name: str = img[7]
+            if len(name) != 40 or not name.startswith("FormXob."): continue
+            qr_rect = pg.get_image_bbox(img)
+            
+            xref: int = img[0]
+            img_dict: dict = donor_doc.extract_image(xref)
+            qr_bytes = img_dict["image"]
+            break
 
-    translated_doc[poster_i].insert_image(qr_rect, stream=qr_bytes)
-translated_doc.save("test_bin/translated_multi_poster.pdf")
+        translated_doc[poster_i].insert_image(qr_rect, stream=qr_bytes)
+        print("Added qr to translated")
+    
+    return translated_doc.tobytes()
 
 # QR png format:
 # FormXob._32_charachter_hash_of_qr_code__.png
